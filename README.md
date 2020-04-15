62b4cced29991566a666d4b14619a0f86ef120a0
HuangJiajia <15602215864@163.com>
2020-04-16 01:36:36 +0800

    提交登录注册修改退出等与user相关的前后端代码。

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # login-register-logout
 Node.js+Mysql+Vue+ElementUI 实现登录注册注销功能
+
---
+++ service/api/userApi.js
@@ -0,0 +1,135 @@
+var models = require('../db/db');
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var $sql = require('../db/sqlMap');
+
+var conn = mysql.createConnection(models.mysql);
+
+conn.connect();
+
+var jsonWrite = function(res, ret) {
+    if(typeof ret === 'undefined') {
+        res.send('err');
+    } else {
+        console.log(ret);
+        res.send(ret);
+    }
+}
+
+// 增加用户接口
+router.post('/addUser', (req, res) => {
+    var sql = $sql.user.add;
+    var params = req.body;
+    params.sex=(params.sex=='woman'?2:1)
+    conn.query(sql, [params.name, params.account, params.pass, params.checkPass,
+                    params.email, params.phone, params.card, params.birth, params.sex], function(err, result) {
+        if (err) {
+            console.log(err);
+        }
+        if (result) {
+            jsonWrite(res, result);
+        }
+    })
+});
+
+//查找用户接口
+router.post('/login', (req, res) => {
+    var sql_name = $sql.user.select_name;
+    var params = req.body;
+    console.log(params)
+    if (params.name) {
+        sql_name += " where account ='"+ params.name +"'";
+    } 
+    console.log(sql_name)   
+    //var keywords = JSON.parse(Object.keys(params)[0]);
+    conn.query(sql_name, params.name, function(err, result) {
+        if (err) {
+            console.log(err);
+        }
+        if (result[0] === undefined) {
+            res.send('-1')   //查询不出username，data 返回-1
+        } else {
+            console.log(result)
+            var resultArray = result[0];
+            if(resultArray.password === params.password) {
+                jsonWrite(res, result);
+            } else {
+                res.send('0')   //username
+            }
+        }
+    })
+});
+
+//获取用户信息
+router.get('/getUser', (req, res) => {
+    var sql_name = $sql.user.select_name;
+    // var sql_password = $sql.user.select_password;
+    var params = req.query;
+    if (params.name) {
+        sql_name += " where account = '"+ params.name + "'";
+    }
+    conn.query(sql_name, params.name, function(err, result) {
+        if (err) {
+            console.log(err);
+        }
+        if (result[0] === undefined) {
+            res.send('-1')   //查询不出username，data 返回-1
+        } else {
+            jsonWrite(res, result);
+        }
+    })
+});
+
+//更新用户信息
+router.post('/updateUser', (req, res) => {
+    var sql_update = $sql.user.update_user;
+    var params = req.body;
+    params.sex=(params.sex=='woman'?2:1)
+    console.log(params);
+    if (params.username) {
+        sql_update  += " email = '" + params.email +
+                        "',phone = '" + params.phone +
+                        "',card = '" + params.card +
+                        "',birth = '" + params.birth +
+                        "',sex = '" + params.sex +
+                        "' where account ='"+ params.username + "'";
+    }    
+    conn.query(sql_update, params.username, function(err, result) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(result);
+        if (result.affectedRows === undefined) {
+            res.send('更新失败，请联系管理员')   //查询不出username，data 返回-1
+        } else {
+            res.send('ok'); 
+        }
+    })
+});
+
+//更改密码
+router.post('/modifyPassword', (req, res) => {
+    var sql_modify = $sql.user.update_user;
+    var params = req.body;
+    console.log(params);
+    if (params.username) {
+        sql_modify +=  " password = '" + params.pass +
+                        "',repeatPass = '" + params.checkPass +
+                        "' where account ='"+ params.username + "'";
+    }
+    conn.query(sql_modify, params.username, function(err, result) {
+        if (err) {
+            console.log(err);
+        }
+        // console.log(result);
+        if (result.affectedRows === undefined) {
+            res.send('修改密码失败，请联系管理员')   //查询不出username，data 返回-1
+        } else {
+            res.send('ok'); 
+        }
+    })
+});
+
+
+module.exports = router;
